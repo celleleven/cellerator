9329472a1b97e7d8281d631beca23b03945a4dd3
S James Parsons Jr <sjamesparsonsjr@gmail.com>
2021-01-07 10:34:05 -0500

    italizize

---
+++ code/Protocol File Architecture/Protocol File Architecture.py
@@ -0,0 +1,6 @@
+
+
+# Incubate Yeast at 30°C for 72 hours; <-- Human readable
+## Incuabte(location, [temperture/time]) <-- Function
+# location; Cellerator || Bioreactor || HotPlate
+# Tempertrue/time; is a comma seperated matrix of celcius / time.
